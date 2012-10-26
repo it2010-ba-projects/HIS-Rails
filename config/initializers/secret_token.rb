af1069f3b481013fff0a979886f3079e3ba5530e
Silvio Wehner <silvio-wehner@gmx.de>
2012-10-26 13:14:04 +0200

    Initiales Railsprojekt [fixes #38479079]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HISRails::Application.config.secret_token = '67fbb82fe5f6c42e98f2339bb28452a4adac0881898447f5f47ae2d6bb379e1ea14d3d99027541f75e68a510d5f87d44930287c0174ce3f25b4e54c536f9a758'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HISRails::Application.config.session_store :cookie_store, key: '_HIS-Rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HISRails::Application.config.session_store :active_record_store
